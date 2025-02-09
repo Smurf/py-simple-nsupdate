2e4d6ef63385f51b06fb3628b8ca668dcaac5397
Smurf <me@smurf.codes>
2025-02-08 23:54:01 -0600

    Fixed import after name change

--- src/simple_nsupdate/__init__.py
+++ src/simple_nsupdate/__init__.py
@@ -1,2 +1,2 @@
-from .simplensupdate import DNSRecord, DNSRecordType, NSUpdater
+from ._simplensupdate import DNSRecord, DNSRecordType, NSUpdater
 __all__ = ['DNSRecord', 'DNSRecordType', 'NSUpdater']
